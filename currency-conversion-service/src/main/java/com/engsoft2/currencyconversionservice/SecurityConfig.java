b10768cc3f44df707189de8c013884ede1a826cc
Rodrigo Goncalves <rodrigogoncalves2408@gmail.com>
2025-11-11 16:51:26 -0300

    Subindo arquivos com configuração de segurança

---
+++ currency-conversion-service/src/main/java/com/engsoft2/currencyconversionservice/SecurityConfig.java
@@ -0,0 +1,27 @@
+package com.engsoft2.currencyconversionservice;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.config.annotation.web.configurers.oauth2.server.resource.OAuth2ResourceServerConfigurer;
+
+@Configuration
+@EnableMethodSecurity
+public class SecurityConfig {
+
+    @Bean
+    SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception{
+        http.authorizeHttpRequests(auth -> 
+        auth.requestMatchers("/admin/**").hasRole("ADMIN")
+        .requestMatchers("/currency-conversion-service/**").hasAnyRole("USER", "ADMIN")
+        .anyRequest().authenticated()
+        )
+        .oauth2ResourceServer(OAuth2ResourceServerConfigurer :: jwt);
+
+    
+        return http.build();
+    }
+
+}
